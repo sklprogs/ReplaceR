b34be21bc06384e51a0cd6bb0eceacf530d9148c
skl.progs <skl.progs@gmail.com>
2017-10-16 21:05:04 +0300

    Add building scripts

---
+++ build/update_here.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -rvu /usr/local/bin/shared/resources .
+cp -rvu /usr/local/bin/ReplaceR/locale .
+
+cp -vu /usr/local/bin/{shared.py,sharedGUI.py,regexp.ru,gettext_windows.py} .
+cp -vu /usr/local/bin/ReplaceR/{dic.txt,in.txt,out.txt,replacer.py,LICENSE,README,README-RU} .
---
+++ build/win/setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict (packages = []
+                    ,includes = []
+                    ,excludes = []
+                    )
+
+executables = [Executable ('replacer.py'
+                          ,base       = 'Win32GUI'
+                          ,targetName = 'replacer.exe'
+                          )
+              ]
+
+setup (name        = 'ReplaceR'
+      ,version     = '1'
+      ,description = 'Simple batch replace in text files/clipboard'
+      ,options = dict(build_exe=buildOptions)
+      ,executables = executables
+      )
